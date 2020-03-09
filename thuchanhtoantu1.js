d8f6cec2a7677b51091f74931bd431e88594cc87
Your Name <you@example.com>
2020-03-09 09:50:21 +0700

    thuchanhtoantu

---
+++ thuchanhtoantu1.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+inputwidth = prompt("nhập chiều dài");
+inputheight = prompt("nhập chiều rộng");
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width*height;
+document.write("diện tích hình chữ nhật:"+ area);
